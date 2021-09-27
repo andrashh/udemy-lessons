9c5e72ddb169b679849002735c63a053b271bf65
Andrey <andrashh@yandex.ru>
2021-09-27 18:49:28 +0700

    First App update

--- script.js
+++ script.js
@@ -61,12 +61,26 @@ const personalMovieDb = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов', ''),
-    d = prompt('На сколько оцените его?', '');
+for ( let i = 0; i < 2; i++ ) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+    if ( a != null && b != null && a != '' & b != '' && a.length < 50 ) {
+        personalMovieDb.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+if ( personalMovieDb.count < 10 ) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDb.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDb);
