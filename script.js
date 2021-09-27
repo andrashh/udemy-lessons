98d4f7be8d6844b6d484d9e857cd32a3a5aacdc5
Andrey <andrashh@yandex.ru>
2021-09-27 16:02:00 +0700

    Create App Movies

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 "use strict";
-let a = 15;
-console.log(a);
 let number = 5; //переменная может меняться
 const leftBorderWidth = 1; // постоянная переменная
 console.log(number + leftBorderWidth);
@@ -50,3 +48,25 @@ let incr = 10,
     decr = 10;
 console.log(++incr);
 console.log(--decr);
+
+
+//# Приложение 1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
