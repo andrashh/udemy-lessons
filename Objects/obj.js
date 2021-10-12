f98dbaf71c12d22d5508ee52f1226761db942dbe
Andrey <andrashh@yandex.ru>
2021-10-12 19:15:15 +0700

    Objects add

---
+++ Objects/obj.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function () {
+        console.log('test');
+    }
+}
+
+options.makeTest();
+
+console.log(Object.keys(options).length);
+
+/*console.log(options.name);
+
+delete options.name;
+
+console.log(options);*/
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
