6eaad403cf9f671f461a182314489558909c4e6c
Andrey <andrashh@yandex.ru>
2021-11-09 18:10:32 +0700

    Page Selectors

---
+++ Page elements/page.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button')[1];
+console.log(btns);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('');
