ebfc5fca309d4bbe629dc04da65034af2425ae7e
Andrey <andrashh@yandex.ru>
2021-11-23 14:37:35 +0700

    Function Construct

--- JavaScript in work/food/js/script.js
+++ JavaScript in work/food/js/script.js
@@ -90,4 +90,49 @@ window.addEventListener('DOMContentLoaded', () => {
           }
      }
      setClock('.timer', deadLine);
+
+     // Modal
+
+     const modalTrigger = document.querySelectorAll('[data-modal]'),
+         modal = document.querySelector('.modal'),
+         modalCloseBtn = document.querySelector('[data-close]');
+
+     function openModal() {
+          modal.classList.toggle('show');
+          document.body.style.overflow = 'hidden';
+          clearInterval(modalTimerId);
+     }
+
+     function closeModal() {
+          modal.classList.toggle('show');
+          document.body.style.overflow = '';
+     }
+
+     modalCloseBtn.addEventListener('click', closeModal);
+
+     modalTrigger.forEach(btn => {
+          btn.addEventListener('click', openModal);
+     });
+
+     modal.addEventListener('click', (e) => {
+          if (e.target === modal) {
+               closeModal();
+          }
+     });
+
+     document.addEventListener('keydown', (e) => {
+          if (e.code === 'Escape' && modal.classList.contains('show')) {
+               closeModal();
+          }
+     });
+
+     const modalTimerId = setInterval(openModal, 3000);
+
+     function showModalByScroll() {
+          if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+               openModal();
+               window.removeEventListener('scroll', showModalByScroll);
+          }
+     }
+     window.addEventListener('scroll', showModalByScroll);
 });
